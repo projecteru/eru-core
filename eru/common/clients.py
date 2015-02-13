80479d429a1a557704fdf10cbbc89c665e85904d
CMGS <ilskdw@gmail.com>
2015-02-13 15:57:31 +0800

    add redis clients

--- eru/common/clients.py
+++ eru/common/clients.py
@@ -3,6 +3,7 @@
 import types
 import etcd
 import docker
+import redis
 from eru.common import settings
 
 
@@ -18,7 +19,15 @@ def get_etcd_client(addr):
         host, port = addr.split(':')
         return etcd.Client(host, int(port))
     raise ValueError('etcd addr must be tuple or string')
-    
+
+def get_redis_client():
+    pool = redis.ConnectionPool(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        max_connections=settings.REDIS_POOL_SIZE,
+    )
+    return redis.Redis(connection_pool=pool)
 
 etcd_client = get_etcd_client(settings.ETCD_MACHINES)
+rdb = get_redis_client()
 
