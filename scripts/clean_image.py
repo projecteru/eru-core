8724a9fbe26d6a24addbc6042c590d6a6a0c1b18
tonic <tonicbupt@gmail.com>
2015-09-02 11:34:32 +0800

    clean image script

---
+++ scripts/clean_image.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+from functools import wraps
+
+from eru.app import create_app_with_celery
+from eru.models import Host
+from eru.config import DOCKER_REGISTRY
+from eru.clients import get_docker_client
+
+
+def with_app_context(f):
+    @wraps(f)
+    def _(*args, **kwargs):
+        app, _ = create_app_with_celery()
+        with app.app_context():
+            return f(*args, **kwargs)
+    return _
+
+
+def need_to_delete_container(image, name):
+    ps = name.split('_')
+    return not (image.startswith(DOCKER_REGISTRY) and len(ps) == 3)
+
+
+@with_app_context
+def clean_image():
+    for host in Host.query.all():
+        client = get_docker_client(host.addr)
+        if not client:
+            continue
+
+        try:
+            client.ping()
+        except:
+            print 'client not connected, skipped.'
+            continue
+
+        for c in client.containers(all=True):
+            if need_to_delete_container(c['Image'], c['Names'][0]):
+                c.remove_container(c['Id'])
+                print 'container %s cleaned.' % c['Names'][0]
+
+        for i in client.images():
+            c.remove_image(i['RepoTags'][0])
+            print 'image %s cleaned.' % i['RepoTags'][0]
+
+        print 'host %s cleaned.' % host.ip
+
+
+if __name__ == '__main__':
+    clean_image()
