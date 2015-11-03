5d588aa6494c791342be2f0cab61893fc0b9b7ba
tonic <tonicbupt@gmail.com>
2015-11-03 11:17:44 +0800

    add task log api

--- eru/api/task.py
+++ eru/api/task.py
@@ -1,12 +1,14 @@
 # coding: utf-8
 
+import json
 from flask import abort
-
 from eru.models import Task
-
+from eru.clients import rds
 from .bp import create_api_blueprint
 
+
 bp = create_api_blueprint('task', __name__, url_prefix='/api/task')
+
 
 @bp.route('/<task_id>/')
 def get_task(task_id):
@@ -14,3 +16,12 @@ def get_task(task_id):
     if not task:
         abort(404, 'Task %s not found' % task_id)
     return task
+
+
+@bp.route('/<task_id>/log/')
+def task_log(task_id):
+    task = Task.get(task_id)
+    if not task:
+        abort(404, 'Task %s not found' % task_id)
+
+    return [json.loads(l) for l in rds.lrange(task.log_key, 0, -1)]
