123de48f38ba80fbbd86522091f56337d8122b78
tonic <tonicbupt@gmail.com>
2015-12-29 10:32:24 +0800

    config in network provider

--- eru/ipam/__init__.py
+++ eru/ipam/__init__.py
@@ -1,5 +1,12 @@
 # coding: utf-8
 
+from eru.config import NETWORK_PROVIDER
 from eru.ipam.macvlan import MacVLANIPAM
+from eru.ipam.calico import CalicoIPAM
 
-ipam = MacVLANIPAM()
+_providers = {
+    'macvlan': MacVLANIPAM,
+    'calico': CalicoIPAM,
+}
+
+ipam = _providers[NETWORK_PROVIDER]()
